f77429da690e46be934b22ea7fe85ae204317742
Eduardo C. de Jesus <edududu221@gmail.com>
2023-01-29 19:09:29 -0300

    Create: - "Home.tsx" - "bgHome.gif" Update: - Estrutura do component "App.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar';
+import Aboult from './components/pages/Aboult';
 import Home from './components/pages/Home';
 
 import "./styles/global.css"
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="text-slate-50 w-[90%] m-auto ">
-      <BrowserRouter>
-        <Navbar/>
+        <header>
+          <Navbar/>
+        </header>
 
-        <Routes>
-          <Route path='/' element={<Home/>} />
-        </Routes>
-      </BrowserRouter>
+        <main>
+          <Home/>
+        </main>
     </div>
   )
 }
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,16 @@
 
 
 export default function Home(){
-    return <h1>Escreva uma home aqui!</h1>
+    return (
+        <div className="text-center mt-40 mb-[25rem]
+        ">
+            <img src="/bgHome.gif" className="absolute z-[-1] left-0 top-0 w-[100%] brightness-75" alt="Fundo noturno com diversos monitores" />
+
+            <div id="home" className="text-2xl font-semibold">
+                <p>Olá, meu nome é</p>
+                <h1 className="uppercase text-[#00529C] text-5xl font-bold stroke-slate-50 brightness-200">Eduardo Cardoso</h1>
+                <p>sou Desenvolvedor Front-end</p>
+            </div>
+        </div>
+)
 }
