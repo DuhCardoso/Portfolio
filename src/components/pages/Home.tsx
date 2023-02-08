38ec715f737ae47d609b4dac511faaa616039d89
Eduardo C. de Jesus <edududu221@gmail.com>
2023-02-07 22:27:56 -0300

    Update: - "Home.tsx" span tag creating and receiving img tag - "Home.tsx" removing frist <p>, keeping only <h1> & second <p> in the div

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,14 +2,22 @@
 
 export default function Home(){
     return (
-        <div className="text-center mt-40 mb-[25rem]
+        <div id="Home" className="text-center pt-[40%] h-screen
         ">
-            <img src="/bgHome.gif" className="absolute z-[-1] left-0 top-0 w-[100%] brightness-75" alt="Fundo noturno com diversos monitores" />
+            <span className="
+                absolute z-[-1] left-0 top-0 w-[100%] 
+                
+            ">
+                <img src="/bgHome.gif"  alt="Fundo noturno com diversos monitores" 
+                className="
+                    w-[100%] h-screen brightness-[.8]
+                    lg:m-auto lg:w-[1137px] 
+                "/>
+            </span>
 
-            <div id="home" className="text-2xl font-semibold">
-                <p>Olá, meu nome é</p>
-                <h1 className="uppercase text-[#00529C] text-5xl font-bold stroke-slate-50 brightness-200">Eduardo Cardoso</h1>
-                <p>sou Desenvolvedor Front-end</p>
+            <div id="home" className="z-[1] text-2xl font-semibold ">
+                <h1 className="uppercase text-sky-500 text-5xl font-bold stroke-slate-50">Eduardo Cardoso</h1>
+                <h2>sou Desenvolvedor Front-end</h2>
             </div>
         </div>
 )
