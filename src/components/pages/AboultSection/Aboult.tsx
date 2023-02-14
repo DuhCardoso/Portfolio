7ee82843aad42705d7b3b825f2e8473fcb655f19
Eduardo C. de Jesus <edududu221@gmail.com>
2023-02-14 17:03:05 -0300

    Update: - "Aboult.tsx" criação do  width das box para as tag's

--- src/components/pages/AboultSection/Aboult.tsx
+++ src/components/pages/AboultSection/Aboult.tsx
@@ -12,7 +12,7 @@ export default function Aboult() {
             </h2>
 
             <p className="
-                opacity-90 mb-7
+                opacity-90 mb-7 w-[95%] mx-auto lg:w-[64rem]
             ">
                 Eu sou Eduardo Cardoso, também conhecido como DuhCardoso. Sou desenvolvedor web, sou muito dedicado a minha área de atuação. Atualmente estou em dedicando a melhor meus conhecimentos de Front & Back-end com muito empenho e paixão.
             </p>
@@ -21,32 +21,32 @@ export default function Aboult() {
 
 
                 <h3 className="
-                    font-semibold text-xl mb-2
+                    font-semibold text-xl mb-2 w-[90%] mx-auto lg:w-[60rem] 
                 ">
                     Web Developer.
                 </h3>
 
                 <p className="
-                    opacity-90 mb-12 
+                    opacity-90 mb-12 w-[90%] mx-auto lg:w-[60rem]
                 ">
                     Como uma pessoa motivada e ambiciosa, tenho vontade de sempre alcançar o melhor e aumentar meus conhecimentos. Sendo comunicativo e buscando o melhor relacionamento com meu colegar, sou capaz de trabalhar como parte de uma equipe e também de forma independente.
                 </p>
 
                 <div className="
-                    flex flex-col mb-12 
+                    flex flex-col mb-12 w-[90%] mx-auto
                     sm:flex-row sm:gap-5 
                     sm:justify-center
-                    lg:gap-16 lg:justify-start
+                    lg:gap-16 lg:w-[60rem]
                 ">
                     <img src="/aboultProfile.png" className="
-                        w-[200px] rounded-lg   object-contain
+                        w-[200px] rounded-lg   object-contain 
                     "/>
 
                     <AboultList />
                 </div>
 
                 <p className="
-                    opacity-90 mb-6
+                    opacity-90 mb-6 w-[90%] mx-auto lg:w-[60rem]
                 ">
                     Minha experiência e habilidades me tornam um ativo valioso em qualquer organização e estou ansioso para assumir novos desafios e responsabilidades. Quer esteja lidando com problemas complexos, desenvolvendo soluções inovadoras ou colaborando com outras pessoas para atingir um objetivo comum, sempre me esforço para oferecer resultados excepcionais.
                 </p>
